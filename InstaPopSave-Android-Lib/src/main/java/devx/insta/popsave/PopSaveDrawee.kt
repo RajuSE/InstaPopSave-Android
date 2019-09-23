b0904f5626d77ed27a203f79cbd1efe38d038b02
Swapnil <swapnilmusale19@gmail.com>
2019-09-23 15:48:30 +0530

    Simple Drawee Support Added

--- InstaPopSave-Android-Lib/src/main/java/devx/insta/popsave/PopSaveDrawee.kt
+++ InstaPopSave-Android-Lib/src/main/java/devx/insta/popsave/PopSaveDrawee.kt
@@ -11,19 +11,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.Animation
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.view.SimpleDraweeView
 
 
-class PopSave
+class PopSaveDrawee
 @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     RelativeLayout(context, attrs, defStyleAttr) {
 
     //Views
-    var imageView: ImageView? = null
+    var frescoImageView: SimpleDraweeView? = null
     var popSaveRoot: CoordinatorLayout? = null
 
     //Configs
@@ -34,6 +35,7 @@ class PopSave
     private var defaultPopperRootHeight = 250
     private var defaultPopperRootWidth = ViewGroup.LayoutParams.WRAP_CONTENT
     private var shouldMoveDown = false
+    private var isFrescoImage = false
 
     init {
         init(context, attrs)
@@ -42,8 +44,9 @@ class PopSave
     private fun init(context: Context, attributes: AttributeSet?) {
         attributes?.let { _ ->
 
-            inflate(context, R.layout.layout_popsave, this)
-            imageView = findViewById(R.id.myImageView)
+            Fresco.initialize(context)
+            inflate(context, R.layout.layout_popsave_drawee, this)
+            frescoImageView = findViewById(R.id.frescoImageView)
             popSaveRoot = findViewById(R.id.popSaveRoot)
 
             val typedArray = context.obtainStyledAttributes(attributes, R.styleable.popsave, 0, 0)
@@ -57,58 +60,61 @@ class PopSave
             setPopperAreaSize(0, 250)
             setPopperSize(140, 140)
 
-            imageView?.setImageDrawable(ContextCompat.getDrawable(context, srcImage))
+            frescoImageView?.setImageDrawable(ContextCompat.getDrawable(context, srcImage))
 
-            imageView?.setOnClickListener {
+            frescoImageView?.setOnClickListener {
                 onViewClickListener?.onViewClicked()
             }
         }
     }
 
-    fun setPopperCornerRadius(radius: Float): PopSave {
-        imageView?.setImageDrawable(ContextCompat.getDrawable(context, srcImage))
+    fun setPopperCornerRadius(radius: Float): PopSaveDrawee {
+        frescoImageView?.setImageDrawable(ContextCompat.getDrawable(context, srcImage))
         return this
     }
 
+
     var onViewClickListener: OnViewClicked? = null
+
 
     interface OnViewClicked {
         fun onViewClicked()
     }
 
     //config methods
-    fun setPopperSize(width: Int, height: Int): PopSave {
+    fun setPopperSize(width: Int, height: Int): PopSaveDrawee {
         if (height > 0)
             defaultPopperHeight = height
         if (width > 0)
             defaultPopperWidth = width
-        imageView?.layoutParams?.height = defaultPopperHeight
-        imageView?.layoutParams?.width = defaultPopperWidth
+        frescoImageView?.layoutParams?.height = defaultPopperHeight
+        frescoImageView?.layoutParams?.width = defaultPopperWidth
 
-        imageView?.requestLayout()
+        frescoImageView?.requestLayout()
 
         return this
     }
 
-    private fun setPopperAreaSize(width: Int, height: Int): PopSave {
+    fun setPopperAreaSize(width: Int, height: Int): PopSaveDrawee {
         if (height > 0)
             defaultPopperRootHeight = height
         if (width > 0)
             defaultPopperRootWidth = width
-
         popSaveRoot?.layoutParams?.height = defaultPopperRootHeight
         popSaveRoot?.layoutParams?.width = defaultPopperRootWidth
+
         popSaveRoot?.requestLayout()
+
         return this
     }
 
-    fun setPopperImage(drawable: Drawable): PopSave {
-        imageView?.setImageDrawable(drawable)
+    fun setPopperImage(drawable: Drawable): PopSaveDrawee {
+        frescoImageView?.setImageDrawable(drawable)
         return this
     }
 
-    fun setPopperImage(resId: Int): PopSave {
-        imageView?.setImageDrawable(
+    fun setPopperImage(resId: Int): PopSaveDrawee {
+        frescoImageView?.setImageDrawable(
             ContextCompat.getDrawable(context, resId)
         )
         return this
@@ -123,11 +129,11 @@ class PopSave
     }
 
     fun popNow() {
-        imageView?.let {
-            imageView!!.visibility = View.VISIBLE
+        frescoImageView?.let {
+            frescoImageView!!.visibility = View.VISIBLE
             popSaveRoot!!.visibility = View.VISIBLE
 
-            PopSaveAnimUtils.pop(imageView, object : Animation.AnimationListener {
+            PopSaveAnimUtils.pop(frescoImageView, object : Animation.AnimationListener {
                 override fun onAnimationStart(animation: Animation) {}
 
                 override fun onAnimationEnd(animation: Animation) {
@@ -140,8 +146,7 @@ class PopSave
         }
     }
 
-
-    fun shouldMoveDown(): PopSave {
+    fun shouldMoveDown(): PopSaveDrawee {
         shouldMoveDown = true
         return this
     }
@@ -149,36 +154,40 @@ class PopSave
     private fun setupPopperGravity() {
         val layoutParams = LinearLayout.LayoutParams(defaultPopperWidth, defaultPopperHeight)
         layoutParams.gravity = if (shouldMoveDown) Gravity.TOP else Gravity.BOTTOM
-        imageView?.layoutParams
-        imageView?.requestLayout()
+        frescoImageView?.layoutParams
+        frescoImageView?.requestLayout()
         requestLayout()
     }
 
-    fun shouldMoveUp(): PopSave {
+    fun loadImage(imageUrl: String, resizeWidth: Int = 100, resizeHeight: Int = 100):PopSaveDrawee{
+        frescoImageView.loadImage(imageUrl, resizeWidth, resizeHeight)
+        return this
+    }
+
+    fun shouldMoveUp(): PopSaveDrawee {
         shouldMoveDown = false
         return this
     }
 
     //animations
     private fun slideUpImageAnimation() {
-        if (true) {
-            imageView?.let {
-                imageView!!.animate()
-                    .setInterpolator(AccelerateDecelerateInterpolator())
-                    .translationY(if (shouldMoveDown) defaultPopperRootHeight.toFloat() else -defaultPopperRootHeight.toFloat())
-                    .setListener(object : AnimatorListenerAdapter() {
-                        override fun onAnimationEnd(animation: Animator) {
-                            super.onAnimationEnd(animation)
-                            imageView?.visibility = View.GONE
-                            popSaveRoot?.visibility = View.GONE
-                            onFinish?.onFinish()
-                            imageView?.animate()?.translationY(0.toFloat())?.setListener(null)
-                        }
-                    })
-            }
-            return
+        frescoImageView?.let {
+            frescoImageView!!.animate()
+                .setInterpolator(AccelerateDecelerateInterpolator())
+                .translationY(if (shouldMoveDown) defaultPopperRootHeight.toFloat() else -defaultPopperRootHeight.toFloat())
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        super.onAnimationEnd(animation)
+                        frescoImageView?.visibility = View.GONE
+                        popSaveRoot?.visibility = View.GONE
+                        onFinish?.onFinish()
+                        frescoImageView?.animate()?.translationY(0.toFloat())?.setListener(null)
+                    }
+                })
         }
+        return
     }
+
 
     interface OnFinish {
         fun onFinish()
@@ -186,8 +195,9 @@ class PopSave
 
     var onFinish: OnFinish? = null
 
-    fun setOnFinishListener(onFinish: OnFinish): PopSave {
+    fun setOnFinishListener(onFinish: OnFinish): PopSaveDrawee {
         this.onFinish = onFinish
         return this
     }
+
 }
